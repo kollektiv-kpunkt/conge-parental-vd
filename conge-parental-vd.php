0c6f675331d2f37b05814ae9d012b22450ae4fe6
timothyoesch <54284649+timothyoesch@users.noreply.github.com>
2022-03-04 09:28:42 +0100

    Activation/Deactivation Hook | Shortcode

--- conge-parental-vd.php
+++ conge-parental-vd.php
@@ -1,7 +1,5 @@
 <?php
 
-namespace CongeParentalVD;
-
 /**
  * The plugin bootstrap file
  *
@@ -15,9 +13,9 @@ namespace CongeParentalVD;
  * @package           Coge Parental VD
  *
  * @wordpress-plugin
- * Plugin Name:       conge-parental-vd
+ * Plugin Name:       Cogé Parental VD
  * Plugin URI:        https://github.com/kollektiv-kpunkt/conge-parental-vd
- * Description:       demovox is a tool to collect signatures for Swiss popular initiatives by offering the visitor a personalized signature sheet.
+ * Description:       Plugin for cantonal initiative in the état de vaud for collecting signatures with Demovox (https://github.com/spschweiz/demovox).
  * Version:           1.0.0
  * Author:            Timothy@K.
  * Author URI:        https://kpunkt.ch/
@@ -31,3 +29,62 @@ namespace CongeParentalVD;
 if (!defined('WPINC')) {
 	die('No direct access');
 }
+
+/**
+ * Currently plugin version.
+ * Start at version 1.0.0 and use SemVer - https://semver.org
+ * Rename this for your plugin and update it as you release new versions.
+ */
+define('CONGEPARENTALVD_VERSION', '1.0.0');
+
+function cpv_install() {
+	global $wpdb;
+
+	$table_name = $wpdb->prefix . 'cpv_boegen';
+
+	$charset_collate = $wpdb->get_charset_collate();
+
+	$sql = "CREATE TABLE $table_name (
+		ID bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+        guid char(36) NOT NULL,
+        first_name varchar(678) NOT NULL,
+        last_name varchar(678) NOT NULL,
+        mail varchar(424) NOT NULL,
+        street varchar(422) NULL,
+        zip varchar(200) NULL,
+        city varchar(296) NULL,
+        is_optin tinyint(4) NULL,
+        is_sent tinyint(4) DEFAULT 0,
+        PRIMARY KEY (ID),
+        UNIQUE KEY guid_index (guid)
+	) $charset_collate;";
+
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+	dbDelta( $sql );
+
+	add_option( 'conge-parental-vd_db_version', "1.0.0" );
+}
+
+
+function cpv_uninstall() {
+    global $wpdb;
+    $table_name = $wpdb->prefix . 'cpv_boegen';
+
+    $wpdb->query("DROP TABLE IF EXISTS $table_name");
+    delete_option( 'conge-parental-vd_db_version');
+}
+
+
+register_activation_hook( __FILE__, 'cpv_install' );
+register_uninstall_hook( __FILE__, 'cpv_uninstall');
+
+function cpv_lander() {
+
+    //Turn on output buffering
+    ob_start();
+    include(__DIR__ . "/views/lander.php");
+    return ob_get_clean();
+}
+
+// register shortcode
+add_shortcode('cpv-lander', 'cpv_lander');
